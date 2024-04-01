c0d2f10a9bf5abb721014e3c1f1ca9c9657f0fea
paynesc1 <paynesc1@gmail.com>
2024-04-01 12:32:00 -0600

    Added Problem 4

---
+++ server.py
@@ -0,0 +1,51 @@
+# generate random list of numbers
+import random
+def randoms():
+    # make list n long, must be at least digits ling
+    my_num = random.randint(2,20)
+    rand_list = []
+    for i in range(my_num):
+        # items in list randonly chosen between 1-5
+        n = random.randint(1,5)
+        rand_list.append(n)
+    return rand_list
+result = randoms()
+print(result)
+
+
+
+# # 1. Write a Python program to sum all the items in a list.
+# def sum(list):
+#     sum = 0
+#     for i in list:
+#         sum += i
+#     return sum
+# print(sum(result))
+
+# 2. Write a Python program to multiply all the items in a list.
+# def multiply(list):
+#     total = 1
+#     for i in list:
+#         total *= i
+#     return total
+# print(multiply(result))
+
+
+# 3. Write a Python program to get the largest number from a list.
+# def largest(list):
+#     largest_num = None
+#     for i in list:
+#         if largest_num == None or i > largest_num:
+#             largest_num = i
+#     return largest_num
+# print(largest(result))
+
+
+# 4. Write a Python program to get the smallest number from a list.
+def smallest(list):
+    smallest = None
+    for i in list:
+        if smallest == None or i < smallest:
+            smallest = i
+    return smallest
+print(smallest(result))
--- test_server.py
+++ test_server.py
@@ -1,18 +1,60 @@
 # pip install pytest
-from server import multiply
+from server import smallest
 
+# ***************************************** #
 
-def test_multiply():
-    # Define your test cases as a list of tuples (input_list, expected_result)
-    test_cases = [
-        ([2, 3, 4], 24),
-        ([1, 2, 3, 4, 5], 120),
-        ([1, -1, 1, -1], 1),
-        ([10], 10),
-        ([], 1)  # Testing multiplication with an empty list should return the identity element, 1
-    ]
+ # 1. Write a Python program to sum all the items in a list.
+# def test_sum():
+#     test_cases = [
+#         ([5,3], 8),
+#         ([-4,-6],-10),
+#         ([7,0], 7),
+#         ([999999,1], 1000000),
+#     ]
+#     for input_list, expected in test_cases:
+#         assert sum(input_list) == expected, f"Add ({input_list}) should equal ({expected})"
+
+# ***************************************** #
+
+# 2. Write a Python program to multiply all the items in a list.
+# def test_multiply():
+#     # Define your test cases as a list of tuples (input_list, expected_result)
+#     test_cases = [
+#         ([2, 3, 4], 24),
+#         ([1, 2, 3, 4, 5], 120),
+#         ([1, -1, 1, -1], 1),
+#         ([10], 10),
+#         ([], 1)  # Testing multiplication with an empty list should return the identity element, 1
+#     ]
     
-    # Iterate over each test case
+#     # Iterate over each test case
+#     for input_list, expected in test_cases:
+#         # Use assert statement to check if multiply function's output matches the expected result
+#         assert multiply(input_list) == expected, f"multiply({input_list}) should equal {expected}"
+
+# ***************************************** #
+
+# 3. Write a Python program to get the largest number from a list.
+# def test_largest():
+#     test_cases = [
+#         ([2, 3, 4], 4),
+#         ([1, 2, 3, 4, 5], 5),
+#         ([1, -1, 1, -1], 1),
+#         ([10], 10),
+#     ]
+#     for input_list, expected in test_cases:
+#         assert largest(input_list) == expected, f"Largest in ({input_list}) should be ({expected})"
+
+# ***************************************** #
+
+# 4. Write a Python program to get the smallest number from a list.
+def test_smallest():
+    test_cases = [
+        ([5, 3, 4, 1, 2], 1),
+        ([-2, -6, -3, -8, -4], -8),
+        ([1, 2, 3, 4, 5], 1),
+        ([5], 5),
+        ([], None),
+    ]
     for input_list, expected in test_cases:
-        # Use assert statement to check if multiply function's output matches the expected result
-        assert multiply(input_list) == expected, f"multiply({input_list}) should equal {expected}"
+        assert smallest(input_list) == expected, f"Smallest in ({input_list}) should be ({expected})"
